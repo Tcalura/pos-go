a4aa08add62eeaccbccb1c558a3e6caa1afeabf1
Thiago Rossi <thiago.rossi@kyndryl.com>
2025-10-01 20:23:06 -0300

    http-client

---
+++ pacotes importantes/http-client/timeout/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Microsecond}
+	resp, err := c.Get("https://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	// ioutil deprecated: Go 1.16
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
