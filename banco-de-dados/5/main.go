9b57d635e5ee6f1d361e81b404a1a996fb471744
Thiago Rossi <thiago.rossi@kyndryl.com>
2025-10-10 12:47:06 -0300

    has_one

---
+++ banco-de-dados/5/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
+type Product struct {
+	ID           int `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	CategoryID   int
+	Category     Category
+	SerialNumber SerialNumber
+	gorm.Model
+}
+
+type SerialNumber struct {
+	ID        int `gorm:"primaryKey"`
+	Number    string
+	ProductID int
+}
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3307)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{})
+
+	// create category
+	category := Category{Name: "Eletronicos"}
+	db.Create(&category)
+
+	// create product
+	db.Create(&Product{
+		Name:       "Mouse",
+		Price:      1000.00,
+		CategoryID: 1,
+	})
+
+	// create serial number
+	// ligaçao de has_one entre product e serial_number
+	db.Create(&SerialNumber{
+		Number:    "123456",
+		ProductID: 1,
+	})
+
+	var products []Product
+	db.Preload("Category").Preload("SerialNumber").Find(&products)
+	for _, product := range products {
+		println("nome, categoria, serial\n")
+		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
+	}
+}
