3a0717cbe189766983e240520f6a8a6160615aec
Thiago Rossi <thiago.rossi@kyndryl.com>
2025-11-09 16:33:14 -0300

    adds swaggger documentation

--- APIS/cmd/server/main.go
+++ APIS/cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"mod-apis/configs"
+	_ "mod-apis/docs"
 	"mod-apis/internal/entity"
 	"mod-apis/internal/infra/database"
 	"mod-apis/internal/infra/webserver/handlers"
@@ -12,10 +13,28 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// @title Go Expert API Example
+// @version 1.0
+// @description Product API with authentication
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Thiago Rossi
+// @contact.url http://linkedin.com/in/tcalura
+// @contact.email thiagocalura@outlook.com
+
+// @license.name Full Cycle License
+// @license.url hhtp://www.fullcycle.com.br
+
+// @host localhost:8000
+// @BasePath /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	//...
 	configs, err := configs.LoadConfig(".")
@@ -54,6 +73,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/user/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
